cc26eb1d1e88be780946206c6091f57bd87e3ef5
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2026-06-02 16:14:07 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,95 @@
+const express = require("express")
+const crypto = require("crypto")
+
+const app = express() ;
+
+app.use(express.json()) ;
+
+const otpStore = { } ;
+
+
+// otp generator
+
+function generateOTP(email)
+{
+	const otp  = crypto.randomInt(100000 , 1000000 ).toString() ;
+	
+	otpStore[email] = {
+		otp ,
+		expiresAt  : Date.now()  + 5 *60*1000 ,
+	}
+
+	return otp ;
+
+} 
+
+
+//SEND OTP route
+
+
+app.post("/send-otp"  , (req , res)=> {
+
+	const {email} = req.body ;
+
+	const otp  = generateOTP(email) ;
+
+	console.log("The Generated OTP is : " , otp ) ;
+
+	res.json({
+		success : true ,
+		message : "OTP GENERATED"
+		
+	}) ;
+
+})
+
+
+//verify the otp 
+
+app.post("/verify-otp" , (req , res) => {
+
+	const {email , otp} = req.body ;
+	const record  = otpStore[email] ;
+
+	if(!record)
+	{
+	return res.status(400).json({
+	  message : "OTP NOT FOUND",
+	}) ;
+
+	if(Date.now() > expiresAt )
+	{
+		delete otpStore[email] ;
+
+		return res.status(403).json({
+			message : "OTP EXPIRED:(", 
+		}) ;
+	}
+
+	if(record.otp !== otp)
+	{
+		return res.status(403).json({
+			message : "THE PROVIDED OTP WAS NOT FOUND / MAYBE WRONG PLEASE CHECK IT" ,
+		});
+	}
+
+	delete otpStore[email] ;
+
+
+	res.json({
+
+		success : true ,
+		message : "OTP VERIFIED SUCCESSFULLY",
+
+
+	})
+
+}
+
+}) ;
+
+
+app.listen(3000 , (req , res) => 
+{
+	console.log("server ran successfully:)") ;
+} ) ;
